16525f146f1576d88f1ff37b9f94b7bde252b825
gilcierweb <gilcier06@yahoo.com.br>
2016-08-27 22:02:43 -0300

    Added views to create/edit blog post inside the site.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # first-blog-django
 My first blog with django python
+
+#python
+
+django-admin startproject mysite
+
+python manage.py migrate
+
+python manage.py runserver
+
+python manage.py startapp [app_label]
+
+python manage.py startapp blog
+
+python manage.py makemigrations blog
+
+python manage.py migrate blog
+
+python manage.py createsuperuser
+
+python manage.py shell
+
+python manage.py makemigrations blog
+
+python manage.py migrate blog
+
+http://tutorial.djangogirls.org/pt/
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
-from .models import Post
-from .forms import PostForm
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -35,8 +36,33 @@ def post_edit(request, pk):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('blog.views.post_detail', pk=post.pk)
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
-    
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post_pk)
